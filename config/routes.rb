198cccfc744080d38f76701809f77ac4231a1f83
Shane P. Carr <shanepcarr@gmail.com>
2019-11-02 17:06:55 -0700

    intentional failure The action  could not be found for RoomsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :rooms
-  resources :room_messages
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root controller: :rooms, action: :index
+
+  resources :room_messages
+  resources :rooms
 end
