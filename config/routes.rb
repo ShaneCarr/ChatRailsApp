874a6c655f88595b1d2dc0891dc6c4fcc25bde52
Shane Carr <shanepcarr@gmail.com>
2019-11-09 18:28:42 -0800

    c

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root controller: :rooms, action: :index
 
+=begin
+  Any object that you want users to be able to access via URI and perform CRUD (or some subset thereof) operations
+on can be thought of as a resource. In the Rails sense, it is generally a database table which is represented by a model,
+ and acted on through a controller.
+=end
   resources :room_messages
   resources :rooms
 end
