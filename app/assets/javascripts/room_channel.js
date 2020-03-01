5629bc619c35595133dfc7ebdff44be5c5304949
shane carr <shanepcarr@gmail.com>
2020-02-29 22:23:12 -0800

    c

---
+++ app/assets/javascripts/room_channel.js
@@ -0,0 +1,28 @@
+$(function() {
+    // for each of these elements pass the room id and subscribe; configure in show.html.erb
+    $('[data-channel-subscribe="room"]').each(function(index, element) {
+        var $element = $(element),
+            room_id = $element.data('room-id')
+        messageTemplate = $('[data-role="message-template"]');
+
+        $element.animate({ scrollTop: $element.prop("scrollHeight")}, 1000)
+
+        App.cable.subscriptions.create(
+            {
+                channel: "RoomChannel",
+                room: room_id
+            },
+            {
+                received: function(data) {
+                    alert('test')
+                    var content = messageTemplate.children().clone(true, true);
+                    content.find('[data-role="user-avatar"]').attr('src', data.user_avatar_url);
+                    content.find('[data-role="message-text"]').text(data.message);
+                    content.find('[data-role="message-date"]').text(data.updated_at);
+                    $element.append(content);
+                    $element.animate({ scrollTop: $element.prop("scrollHeight")}, 1000);
+                }
+            }
+        );
+    });
+});
